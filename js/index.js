70a8545f0a5fe0600275281e6f95c89b7f6a67ce
GRJesus <rod.giu@hotmail.com>
2019-08-01 21:02:28 -0300

    Ordenacao de valores

---
+++ js/index.js
@@ -0,0 +1,14 @@
+//2) Escrever um programa em Javascript que lê um conjunto de 4 valores i, a, b, c, onde i é um valor inteiro e positivo e a, b, c, são quaisquer valores reais e os escreva. A seguir:
+//a) Se i=1 escrever os três valores a, b, c em ordem crescente.
+//b) Se i=2 escrever os três valores a, b, c em ordem decrescente.
+//c) Se i=3 escrever os três valores a, b, c de forma que o maior entre a, b, c fique dentre os dois.
+
+var value1 = parseInt(prompt("Insira um numero de 1 a 3"));
+var value2 = parseInt(prompt("Insira o primeiro valor"));
+var value3 = parseInt(prompt("Insira o segundo valor"));
+var value4 = parseInt(prompt("Insira o terceiro valor"));
+
+
+if (value1 === 1) {
+  if (value2 < value3 && value4 < value2 && )
+}
